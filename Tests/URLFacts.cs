31735b5d2a478560781dcdf3578b98123022c348
Luke Sampson <me@lukesampson.com>
2012-03-25 10:35:14 +1000

    Added tests for URL

---
+++ Tests/URLFacts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HastyAPI;
+using Xunit;
+
+namespace Tests {
+	public class URLFacts {
+
+		[Fact]
+		public void Add_Operator_Works_On_Non_Root_Base_Paths() {
+			URL basePath = "https://www.test.com/subdir/";
+			Assert.Equal("https://www.test.com/subdir/rel", basePath + "rel");
+		}
+	}
+}
